672280b36ff1aed6e3ec1e7f83438d3e17cccf38
Filipe Forattini <filipe@vitta.me>
2022-12-06 17:03:57 -0300

    test: initial tests for moleculer-db service

---
+++ test/adapter.test.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+
+const { nanoid } = require("nanoid");
+const { BrokerFactory } = require("./concerns");
+
+const RESOURCE_ATTRIBUTES = {
+  title: "My first post",
+  content: "Lorem ipsum...",
+  votes: 0,
+  token: nanoid(),
+};
+
+describe("resources", () => {
+  let broker = BrokerFactory();
+
+  beforeAll(async function () {
+    await broker.start();
+  });
+
+  afterAll(() => {
+    return broker.stop();
+  });
+
+  it("create", async () => {
+    const data = await broker.call("posts.create", RESOURCE_ATTRIBUTES);
+    expect(data.id).toBeDefined();
+  });
+
+  it("create and get", async () => {
+    const originalData = await broker.call("posts.create", RESOURCE_ATTRIBUTES);
+    const liveData = await broker.call("posts.get", { id: originalData.id });
+
+    for (const [key, value] of Object.entries(originalData)) {
+      expect(liveData[key]).toEqual(value);
+    }
+  });
+
+  it("create and remove", async () => {
+    const data = await broker.call("posts.create", RESOURCE_ATTRIBUTES);
+    await broker.call("posts.remove", { id: data.id });
+  });
+
+  it("create, update and get", async () => {
+    const originalData = await broker.call("posts.create", RESOURCE_ATTRIBUTES);
+
+    const updatedData = await broker.call("posts.update", {
+      id: originalData.id,
+      title: "My new title!",
+    });
+
+    console.log({ updatedData });
+
+    const obj = await broker.call("posts.get", { id: originalData.id });
+    const entries = Object.entries(obj).filter(([key]) => !key.startsWith("_"));
+
+    for (const entry of entries) {
+      const [key, value] = entry;
+      expect(updatedData[key]).toEqual(value);
+    }
+  });
+
+  it("list", async () => {
+    const list = await broker.call("posts.list");
+  });
+});
--- test/concerns/index.js
+++ test/concerns/index.js
@@ -5,7 +5,7 @@ jest.setTimeout(30 * 1000);
 const DbService = require("moleculer-db");
 const { ServiceBroker } = require("moleculer");
 
-const S3dbAdapter = require("../../src");
+const {S3dbAdapter} = require("../../src");
 const MoleculerConfig = require("./moleculer.config");
 
 const { AWS_S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
@@ -24,16 +24,10 @@ module.exports = {
 
       adapter: new S3dbAdapter({
         uri: `s3://${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}@${AWS_S3_BUCKET}/databases/moleculer`,
+        passphrase: 'super-secret',
       }),
 
-      resource: {
-        name: "posts",
-        schema: {
-          title: "string",
-          content: "string",
-          votes: "number",
-        },
-      },
+
     });
 
     return broker;
